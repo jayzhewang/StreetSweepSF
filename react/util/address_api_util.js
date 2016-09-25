4666838ddd31c6cd00c1f5cabc836942de3004cf
Zhe Wang <akina.dwnhl@gmail.com>
2016-09-24 18:59:22 -0700

    changed chrome sync API

--- react/util/address_api_util.js
+++ react/util/address_api_util.js
@@ -2,6 +2,6 @@ export const getChromeSyncAPI = successFunction => {
   chrome.storage.sync.get('addresses', successFunction);
 };
 
-export const setChromeSyncAPI = (data) => {
+export const setChromeSyncAPI = data => {
   chrome.storage.sync.set({'addresses': data});
 };
